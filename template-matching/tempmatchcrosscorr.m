321117564949b3df2d9bd9c2e40c4fb5a4f78cdd
Jakob Frantzvåg Karlsmoen <jakob.karlsmoen@hotmail.no>
2021-05-08 13:11:16 +0200

    Updated template matching matlab files

--- template-matching/tempmatchcrosscorr.m
+++ template-matching/tempmatchcrosscorr.m
@@ -1,25 +1,33 @@
-function rectanglePosAndSize = tempmatchcrosscorr(image, template)
+function rectanglePosAndSize = tempmatchcrosscorr(image, template, threshold)
+
 % save size of template for later use
 dimensionsTemplate = size(template);
 
 % calculate "correlation score" between the images
-correlation = normxcorr2(template, image);
+
+% loop that will shrink the template until it fits in image 
+% not pretty but works :)
+error = true;
+while error
+    try
+        correlation = normxcorr2(template, image);
+        error = false;
+    catch
+        template = imresize(template, 0.75);
+    end
+end
+
 
 % find the cooridinates for the highest score 
 % (this will only find the first one even if there are more exact matches)
-[~, index] = max(abs(correlation(:)));
+[value, index] = max(abs(correlation(:)));
 [y, x] = ind2sub(size(correlation),index(1));
 
-% (wack)
-% it seems that the starting x-coordinate for matched area is 
-% negative if it didn't find a good match.
-% (this can wrongly mark a good match as bad if the match is close to left edge)
-%if (x-dimensionsTemplate(2)) < 0
-%    disp("No good match found");
-%    return
-%end
-
-% according to documentation for normxcorr2, some padding that needs to
-% be compensated for when drawing rectangle
-rectanglePosAndSize = [x-dimensionsTemplate(2) y-dimensionsTemplate(1) dimensionsTemplate(2), dimensionsTemplate(1)];
-
+if value >= threshold
+    % according to documentation for normxcorr2, some padding that needs to
+    % be compensated for when drawing rectangle
+    rectanglePosAndSize = [x-dimensionsTemplate(2) y-dimensionsTemplate(1) dimensionsTemplate(2), dimensionsTemplate(1)];
+else
+    % it didn't meet the threshold value, set all rectangle values to -1
+    rectanglePosAndSize = [-1 -1 -1 -1];
+end
